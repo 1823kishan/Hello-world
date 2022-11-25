f2b15bb74c14b4d83abce46288bfd3b1c5f9256f
Kishan Savaliya <110983980+1823kishan@users.noreply.github.com>
2022-11-25 14:22:09 +0530

    Add files via upload

---
+++ tute is 6 set.py
@@ -0,0 +1,9 @@
+#colection of uniq elements.
+#curely bracket is for set{}.
+#it is take random value.
+#ama index support nathi karatu.
+#not support duplicate value.
+s = {22, 25, 14, 21, 5}
+print(s)
+s.add(77)#you can add in and do more your self.
+print(s)
