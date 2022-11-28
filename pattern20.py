f5e2a2e1eacbdfb25cd63077bef9918fb4f9c788
Kishan Savaliya <110983980+1823kishan@users.noreply.github.com>
2022-11-28 16:01:46 +0530

    Add files via upload

---
+++ pattern20.py
@@ -0,0 +1,8 @@
+for i in range(10, 0, -1):
+    for j in range(1, i + 1):
+        print("*", end=" ")
+    print()
+for k in range(5, 0, -1):
+    for l in range(1, k + 1):
+        print("*", end=" ")
+    print()
